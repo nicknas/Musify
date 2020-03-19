7393792f706c633e4c535eab8b779eb7453008d0
Your Name <you@example.com>
2020-03-19 11:57:08 +0100

    First changes in the App

---
+++ src/main/java/com/musify/ClientCredentialsExample.java
@@ -0,0 +1,67 @@
+package com.musify;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.model_objects.specification.AlbumSimplified;
+import com.wrapper.spotify.model_objects.specification.Artist;
+import com.wrapper.spotify.model_objects.specification.TrackSimplified;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+
+import java.io.IOException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+public class ClientCredentialsExample {
+    private static final String clientId = "63cd1c05a2de40b19d4316d23e5271bf";
+    private static final String clientSecret = "e0a096314a2946e4ab0c5a73f9fdd4cd";
+
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(clientId)
+            .setClientSecret(clientSecret)
+            .build();
+    private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
+            .build();
+
+    public static void clientCredentials_Sync() {
+        try {
+            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+
+            // Set access token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+            System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+            Artist[] categories = spotifyApi.searchArtists("beatles").build().execute().getItems();
+            AlbumSimplified[] albums = spotifyApi.getArtistsAlbums(categories[0].getId()).build().execute().getItems();
+            for (AlbumSimplified album : albums) {
+                TrackSimplified[] tracks = spotifyApi.getAlbumsTracks(album.getId()).build().execute().getItems();
+                for (TrackSimplified track: tracks) {
+                    System.out.println(track.getName());
+                }
+            }
+        } catch (IOException | SpotifyWebApiException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void clientCredentials_Async() {
+        try {
+            final CompletableFuture<ClientCredentials> clientCredentialsFuture = clientCredentialsRequest.executeAsync();
+
+            // Thread free to do other tasks...
+
+            // Example Only. Never block in production code.
+            final ClientCredentials clientCredentials = clientCredentialsFuture.join();
+
+            // Set access token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+            System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+        } catch (CompletionException e) {
+            System.out.println("Error: " + e.getCause().getMessage());
+        } catch (CancellationException e) {
+            System.out.println("Async operation cancelled.");
+        }
+    }
+}
